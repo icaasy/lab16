48262cce07cc4b6c634f680acb3d635ff6efaee1
icaasy <yrukdrobotenko@gmail.com>
2024-03-26 09:15:45 +0300

    getNSpecialElement

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -97,5 +97,7 @@ int cmp_long_long(const void *pa, const void *pb);
 int countNUnique(long long *a, int n);
 int countEqClassesByRowsSum(matrix m);
 
+int getNSpecialElement(matrix m);
+
 
 #endif //LAB_MATRIX_H
